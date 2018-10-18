6d9e20da1dc01f4af110cf31615f27f7e1591e88
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2018-10-18 09:10:48 -0400

    generated board

--- ng-battleship/src/app/board.ts
+++ ng-battleship/src/app/board.ts
@@ -1,2 +1,9 @@
-export class Board {
+import { Player } from './player'
+export class Board{
+  player:Player;
+  tiles:Object[];
+
+  constructor(values: Object = {}) {
+    Object.assign(this,values);
+  }
 }
