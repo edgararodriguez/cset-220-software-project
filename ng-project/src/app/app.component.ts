19bf373b1037e36a225877bd223007f54cdf4484
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2018-12-04 16:03:02 -0500

    updated component ts working on game functionality

--- ng-project/src/app/app.component.ts
+++ ng-project/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
 import { ChatService } from './services/chat.service';
+import { ToastrModule } from 'ngx-toastr';
+import { BoardService } from './board.service'
+import { Board } from './board'
+// set game constants
+declare const Pusher: any;
+const NUM_PLAYERS = 2;
+const BOARD_SIZE = 6;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [BoardService]
 })
 export class AppComponent {
 
