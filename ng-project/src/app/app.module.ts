cf5028bc03c6b2d675ede8e39206c1bd27c9560d
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2018-12-10 07:52:13 -0500

    added imports to my app.module.ts file

--- ng-project/src/app/app.module.ts
+++ ng-project/src/app/app.module.ts
@@ -2,27 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { PusherService } from './services/pusher.service';
 import { ChatService } from './services/chat.service';
 import { MyDetailsComponent } from './my-details/my-details.component';
 import { ChatComponent } from './chat/chat.component';
 import { ToastrModule } from 'ngx-toastr';
+import { routing }        from './app.routing';
+import { AlertComponent } from './_components';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { HomeComponent } from './home';
+import { RegisterComponent } from './register';
+import { LoginComponent } from './login';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MyDetailsComponent,
-    ChatComponent
+    ChatComponent,
+    routing
   ],
   imports: [
     ToastrModule.forRoot(),
     BrowserModule,
     FormsModule,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
     HttpClientModule,
+    AlertComponent,
     ReactiveFormsModule
   ],
-  providers: [ChatService, PusherService],
+  providers: [ChatService, PusherService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
