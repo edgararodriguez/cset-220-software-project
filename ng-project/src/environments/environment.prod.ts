a9cdb94f364eff7477f1a2cc4e491ac380fdce79
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2018-12-11 15:42:00 -0500

    placed new api url

--- ng-project/src/environments/environment.prod.ts
+++ ng-project/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000'
+  apiUrl: 'https://whispering-mountain-28600.herokuapp.com/'
 };
--- ng-project/src/environments/environment.ts
+++ ng-project/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000'
+  apiUrl: 'https://whispering-mountain-28600.herokuapp.com/'
 };
 
 /*
