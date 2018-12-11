15e179c2ecb5a59bb1aa2fe6417fb9992361dcc9
edgararodriguez <31549562+edgararodriguez@users.noreply.github.com>
2018-12-11 14:30:34 -0500

    changed routes for login to be the home

--- ng-project/src/app/app.routing.ts
+++ ng-project/src/app/app.routing.ts
@@ -5,12 +5,12 @@ import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 import { AuthGuard } from './_guards';
 const appRoutes: Routes = [
-    { path: '', component: BattleshipComponent },
-    { path: 'login', component: LoginComponent },
+    { path: 'battleship', component: BattleshipComponent },
+    { path: '', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 
     // otherwise redirect to home
-    { path: '**', redirectTo: 'login' }
+    { path: '**', redirectTo: '' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
